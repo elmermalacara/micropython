80d7928dfc8d21744ad5b6273125c2768ba05838
elmermalacara <malacara.elmer@gmail.com>
2026-09-20 17:26:09 -0600

    Update mpconfigboard.cmake
    Signed-off-by: elmermalacara <malacara.elmer@gmail.com>

--- ports/rp2/boards/RPI_PICO2/mpconfigboard.cmake
+++ ports/rp2/boards/RPI_PICO2/mpconfigboard.cmake
@@ -1,9 +1,16 @@
-# cmake file for Raspberry Pi Pico2
+# cmake file for Raspberry Pi Pico2 (Modificado para 16MB)
 set(PICO_BOARD "pico2")
+
+# Configuración de memoria Flash completa (16 MB)
+set(PICO_FLASH_SIZE_BYTES 16777216)
+
+# Divisor de reloj QSPI para compatibilidad con memorias genéricas
+set(PICO_FLASH_SPI_CLKDIV 4)
 
 # To change the gpio count for QFN-80
 # set(PICO_NUM_GPIOS 48)
 
+# Espacio para el sistema de archivos (16MB total - 1MB firmware = 15MB)
 if(NOT DEFINED MICROPY_HW_FLASH_STORAGE_BYTES)
-    set(MICROPY_HW_FLASH_STORAGE_BYTES 3145728)  # 4MB flash - 1MB firmware = 3MB filesystem
+    set(MICROPY_HW_FLASH_STORAGE_BYTES 15728640)
 endif()
